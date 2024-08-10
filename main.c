e1ce1d6d06011622735e2164e96bd96756d52ab6
s-blanco-dev <santiago.blancoc@correo.ucu.edu.uy>
2024-08-10 17:15:53 -0300

    Contenido agregado

--- main.c
+++ main.c
@@ -2,3 +2,37 @@
 
 /* 1er Desafío de programación - Matemática Discreta I */
 /* Santiago Blanco - Grupo M1 */
+
+int main()
+{
+  int terminos = 0;
+  int mayor_num = 0;
+
+  for (int i = 1; i <= 100000; i++)
+  {
+    int terminos_aux = 0;
+    int num = i;
+
+    while (num != 1)
+    {
+      terminos_aux++;
+      if (num % 2 == 0)
+      {
+	num /= 2;
+      }
+      else
+      {
+	num = num * 3 + 1;
+      }
+    }
+
+    if (terminos_aux > terminos)
+    {
+      terminos = terminos_aux;
+      mayor_num = i;
+    }
+  }
+  
+printf("El número inicial que produce la mayor cantidad de términos es %d con un total de %d términos.\n", mayor_num, terminos);
+  return 0;
+}
